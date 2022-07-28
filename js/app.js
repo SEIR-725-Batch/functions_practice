8f18b9a4ffb1e4407be0d849e70b11a04bcafac0
aparks4 <arynmparks@gmail.com>
2022-07-28 11:48:43 -0700

    Complete problem 3

--- js/app.js
+++ js/app.js
@@ -12,3 +12,13 @@ function reverseWordOrder(str) {
 }
 
 //console.log(reverseWordOrder('what is happening here'))
+
+//Problem 3
+function calculate(num1, num2, operation) {
+    if (operation === "add") return num1 + num2;
+    else if (operation === "sub") return num1 - num2;
+    else if (operation === "mult") return num1 * num2;
+    else if (operation === "div") return num1 / num2;
+    else if (operation === "exp") return num1 ** num2;
+}
+//console.log(calculate(4, 3, "exp"))
