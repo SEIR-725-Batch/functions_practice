d8ffdb961ec007e67c1ec1c9048a606ca7f59078
aparks4 <arynmparks@gmail.com>
2022-07-28 11:42:18 -0700

    Complete problem 2

--- js/app.js
+++ js/app.js
@@ -2,4 +2,13 @@
 function printGreeting(name) {
     return(`Hello, there ${name}.`);
 }
-console.log(printGreeting('Bob'));
+//console.log(printGreeting('Bob'));
+
+//Problem 2
+function reverseWordOrder(str) {
+    let words = [];
+    words = str.split(" ");
+    return words.reverse(" ").join(" ")
+}
+
+//console.log(reverseWordOrder('what is happening here'))
