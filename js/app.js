fade9ac862ac7060144e805d920bee68e605ea28
aparks4 <arynmparks@gmail.com>
2022-07-28 19:57:49 -0700

    Complete problem 4

--- js/app.js
+++ js/app.js
@@ -22,3 +22,22 @@ function calculate(num1, num2, operation) {
     else if (operation === "exp") return num1 ** num2;
 }
 //console.log(calculate(4, 3, "exp"))
+
+//Problem 4
+function checkPandigital(num) {
+   num = String(num).split('').sort(function(a, b){return a - b});
+   for (i=0; i<num.length; i++) {
+    if (i == num[i]) {
+        return true;
+    } else if (i + 1 == num[i]) {
+        return true;
+    } else {
+        return false
+    }
+        
+    }
+   }
+
+
+
+//console.log(checkPandigital(7531642))
